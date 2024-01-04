635e4168b84c5177fc7a5ad01222db3ea4eba6d7
Rafael Altieris <raphaelaltieris@gmail.com>
2024-01-04 17:09:43 -0300

    alterações em exercícios

--- js/script.js
+++ js/script.js
@@ -93,3 +93,41 @@ function exercicio4() {
   resp.innerHTML += `O dobro de ${numero} é ${numero * 2} e sua metade é ${numero / 2}`;
 
 }
+
+function exercicio5() {
+  let resp = document.getElementById("resposta");
+  resp.innerHTML = "<h2>Resposta da Atividade</h2>";
+
+  let num1 = document.getElementById("num1").value;
+  let num2 = document.getElementById("num2").value;
+  let soma = parseInt(num1) + parseInt(num2);
+
+  resp.innerHTML += `A soma de ${num1} + ${num2} é: ${soma}`;
+}
+
+function exercicio6() {
+  let resp = document.getElementById("resposta");
+  resp.innerHTML = "<h2>Resposta da Atividade</h2>"
+
+  let numero = document.getElementById("num1").value;
+  let sucessor = parseInt(numero) + 1;
+  let antecessor = parseInt(numero) - 1;
+
+  resp.innerHTML += `o número ${numero} tem o sucessor ${sucessor} e antecessor ${antecessor}`;
+}
+
+function exercicio7() {
+  let resp = document.getElementById("resposta");
+  resp.innerHTML = "<h2>Resposta da Atividade</h2>"
+
+  let produto = document.getElementById("produto").value;
+  let preco = document.getElementById("preco").value;
+  let pago = document.getElementById("pago").value;
+
+  let troco = parseInt(preco) - parseInt(pago);
+
+  resp.innerHTML += `O produto ${produto} custou ${preco} e você pagou ${pago}. O valor do troco é ${troco}`
+
+
+
+}
